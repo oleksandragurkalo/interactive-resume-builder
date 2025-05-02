a4eb1dead980f9762757063eac3fb6f621f513a7
Oleksandra Gurkalo <oleksandra.gurkalo@gmail.com>
2025-05-02 12:21:00 -0400

    update(*) - move App (update structure)

--- resume-builder/src/components/App/App.tsx
+++ resume-builder/src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import PersonalInfoForm from "./components/PersonalInfoForm.tsx";
+import PersonalInfoForm from "../PersonalInfoForm/PersonalInfoForm.tsx";
 
 function App() {
     return (
         <main className="grid grid-cols-6 items-start min-h-screen text-gray-900">
             <div className="col-span-4 col-start-2">
                 <header className="text-center py-6 border-b">
-                    <h1 className="text-3xl font-bold text-blue-700">Interactive Resume Builder</h1>
+                    <h1 className="">Interactive Resume Builder</h1>
                     <p className="text-sm text-gray-500">Start creating your resume step-by-step</p>
                 </header>
 
@@ -18,4 +18,4 @@ function App() {
     )
 }
 
-export default App
+export default App;

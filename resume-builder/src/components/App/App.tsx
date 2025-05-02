cc75c1d81b8416f8046c461ddb819d5cb035f1ae
Oleksandra Gurkalo <oleksandra.gurkalo@gmail.com>
2025-05-02 12:48:18 -0400

    update(*) - update styles

--- resume-builder/src/components/App/App.tsx
+++ resume-builder/src/components/App/App.tsx
@@ -6,13 +6,13 @@ function App() {
         <main className="grid grid-cols-6 items-start min-h-screen text-gray-900">
             <div className="col-span-4 col-start-2">
                 <header className="text-center py-6 border-b">
-                    <h1 className="">Interactive Resume Builder</h1>
+                    <h1 className="text-3xl font-bold text-blue-700">Interactive Resume Builder</h1>
                     <p className="text-sm text-gray-500">Start creating your resume step-by-step</p>
                 </header>
-
-                <section className="p-6 max-w-5xl mx-auto">
+                <div
+                    className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-12 lg:overflow-visible lg:px-0">
                     <PersonalInfoForm/>
-                </section>
+                </div>
             </div>
         </main>
     )

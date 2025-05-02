fdaa6eb298d150ebf73b897c6c52b3d16b2c8f7d
Oleksandra Gurkalo <oleksandra.gurkalo@gmail.com>
2025-05-02 13:10:54 -0400

    add(Header) - add Header component

--- resume-builder/src/components/App/App.tsx
+++ resume-builder/src/components/App/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import PersonalInfoForm from "../PersonalInfoForm/PersonalInfoForm.tsx";
+import Header from "../Header/Header.tsx";
 
 function App() {
     return (
         <main className="grid grid-cols-6 items-start min-h-screen text-gray-900">
             <div className="col-span-4 col-start-2">
-                <header className="text-center py-6 border-b">
-                    <h1 className="text-3xl font-bold text-blue-700">Interactive Resume Builder</h1>
-                    <p className="text-sm text-gray-500">Start creating your resume step-by-step</p>
-                </header>
+                <Header/>
                 <div
                     className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-12 lg:overflow-visible lg:px-0">
                     <PersonalInfoForm/>
---
+++ resume-builder/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="text-center py-6 border-b">
+            <h1 className="text-3xl font-bold text-blue-700">Interactive Resume Builder</h1>
+            <p className="text-sm text-gray-500">Start creating your resume step-by-step</p>
+        </header>
+    )
+}
+
+export default Header;

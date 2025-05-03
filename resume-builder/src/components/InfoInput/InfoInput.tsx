8ebbd418890595ef901d11576f201fbd5bba00a6
Oleksandra Gurkalo <oleksandra.gurkalo@gmail.com>
2025-05-02 20:05:44 -0400

    update(InfoInput) - update markup

--- resume-builder/src/components/InfoInput/InfoInput.tsx
+++ resume-builder/src/components/InfoInput/InfoInput.tsx
@@ -1,19 +1,28 @@
+import {PersonalInfoField} from "../../data/resume.model.tsx";
+
 type InfoInputProps = {
-    name: string,
-    placeholder: string
-    value: string,
+    info: PersonalInfoField;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-function InfoInput( { name, placeholder, value, onChange }: InfoInputProps) {
+function InfoInput( { info, onChange }: InfoInputProps) {
+    console.log('InfoInput rendered', info);
     return (
-        <input
-            name={name}
-            placeholder={placeholder}
-            value={value}
-            onChange={onChange}
-            className="w-full border p-2 border-gray-300 rounded"
-        />
+        <div className="flex flex-col gap-1">
+            <label htmlFor={info.name} className="font-medium text-sm text-gray-700">
+                {info.label}
+            </label>
+            <input
+                type={info.type}
+                name={info.name}
+                id={info.name}
+                placeholder={info.placeholder}
+                required={info.required}
+                value={info.value}
+                onChange={onChange}
+                className="w-full border p-2 border-gray-300 rounded"
+            />
+        </div>
     )
 }
 

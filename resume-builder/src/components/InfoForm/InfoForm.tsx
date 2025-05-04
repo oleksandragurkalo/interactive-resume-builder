28f908131fd4aec2b63226f2f6910208aedf18d0
Oleksandra Gurkalo <oleksandra.gurkalo@gmail.com>
2025-05-03 21:05:35 -0400

    update(InfoForm) - add button Continue

--- resume-builder/src/components/InfoForm/InfoForm.tsx
+++ resume-builder/src/components/InfoForm/InfoForm.tsx
@@ -4,16 +4,33 @@ import { PersonalInfo } from "../../data/resume.model.tsx";
 type InfoFormProps = {
     info: PersonalInfo;
     handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+    handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    fieldErrors: Record<string, string | null>;
 };
 
-function InfoForm ({ info, handleChange }: InfoFormProps) {
-    console.log('InfoForm rendered', info);
+function InfoForm ({ info, handleChange, handleClick, fieldErrors }: InfoFormProps) {
+    const isFormValid = Object.values(fieldErrors).every((error) => error === null);
+
     return (
-        <form className="space-y-4">
-            {Object.values(info).map((field) => (
-                <InfoInput key={field.name} info={field} onChange={handleChange} />
-            ))}
-        </form>
+        <>
+            <form className="space-y-4">
+                {Object.values(info).map((field) => (
+                    <InfoInput key={field.name} info={field} error={fieldErrors[field.name]} onChange={handleChange}/>
+                ))}
+            </form>
+            <div className="flex justify-end">
+                <button type="submit"
+                        disabled={!isFormValid}
+                        onClick={handleClick}
+                        className={`px-5 py-2 rounded-md font-medium transition-colors
+                            ${isFormValid
+                            ? "bg-blue-600 text-white hover:bg-blue-700"
+                            : "bg-gray-300 text-gray-500 cursor-not-allowed"}`}
+                >
+                    Continue
+                </button>
+            </div>
+        </>
     )
 }
 

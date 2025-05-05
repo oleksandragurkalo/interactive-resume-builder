1c4b898abdb8fd44cec257418c11b9425f8620ed
Oleksandra Gurkalo <oleksandra.gurkalo@gmail.com>
2025-05-05 12:44:16 -0400

    update(validation) - add validation for non-required fields

--- resume-builder/src/utils/validation.tsx
+++ resume-builder/src/utils/validation.tsx
@@ -1,9 +1,10 @@
-import { PersonalInfo, PersonalInfoField } from "../data/resume.model.tsx";
+import { PersonalInfo, InputField } from "../data/resume.model.tsx";
 
-export function validateField(field: PersonalInfoField): string | null {
+export function validateField(field: InputField): string | null {
     const value = field.value.trim();
 
     if (field.required && value === "") return "This field is required.";
+    if (!field.required && value === "" && field.type !== "checkbox") return `This field is optional. But it is a best practice to add ${field.label}.`;
     if (field.type === "tel" && !/^\d+$/.test(value)) return "Phone number must contain only digits.";
     if (field.type === "email" && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) return "Invalid email address.";
 
